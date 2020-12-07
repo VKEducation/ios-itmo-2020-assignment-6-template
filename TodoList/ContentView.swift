c81ebf4fd7a88b41188cd12a61feef82d0d55d9e
jonfir <yolchev@gmsil.com>
2020-12-07 12:50:07 +0700

    first commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct TodoListView: View {
+    @StateObject var viewModel = TodoListViewModel()
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
